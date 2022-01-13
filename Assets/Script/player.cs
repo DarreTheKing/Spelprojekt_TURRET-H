e37312c5765b5166b04210f7a92bc397877098f5
Zakk <zakk.norman@elev.ga.lbs.se>
2022-01-13 14:51:27 +0100

    2 scripts om spelaren och scrap
    Ladde till folder för scripts och turrets, lagde även till mina scripts som får spelaren att röra sig och förmågan att plocka upp scrap plus placera ut turrets dock finns det inga turrets att placera ut.

---
+++ Assets/Script/player.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    Vector2 movement;
+    public GameObject turret1;
+    public GameObject turret2;
+    public float scrapAmount = 0;
+    scrapui scrapAmountShow;
+    private float turretAmount;
+    public float health = 5;
+    private GameObject currentTurret;
+    public Sprite turret1Sprite;
+    public Sprite turret2Sprite;
+    public Image turretSelect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        scrapAmountShow = FindObjectOfType<scrapui>();
+        currentTurret = turret1;
+        changeimg();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        scrapAmountShow.playerScrap = scrapAmount;
+
+        if (Input.GetKeyDown(KeyCode.Space) && scrapAmount > 25 && turretAmount < 3)
+        {
+            Instantiate(currentTurret, rb.position, Quaternion.identity);
+            scrapAmount -= 25;
+            turretAmount += 1;
+        }
+        if (health <= 0)
+        {
+            GameObject.Destroy(gameObject);
+            Time.timeScale = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentTurret = turret1;
+            changeimg();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentTurret = turret2;
+            changeimg();
+
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "scrap")
+        {
+            scrapAmount += 15;
+            Destroy(collision.gameObject);
+        }
+    }
+    private void changeimg()
+    {
+        if (currentTurret == turret1)
+        {
+            turretSelect.sprite = turret1Sprite;
+        }
+        if (currentTurret == turret2)
+        {
+            turretSelect.sprite = turret2Sprite;
+        }
+    }
+}
+
