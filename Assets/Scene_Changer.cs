157837aacfbf153e5a6076157ca22e7111bf005e
DarreTheKing <darian.belvert@elev.ga.lbs.se>
2022-01-17 13:10:22 +0100

    blabla

--- Assets/Scene_Changer.cs
+++ Assets/Scene_Changer.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scene_Changer : MonoBehaviour
 {
-    public void PlayGame()
-    { 
+    [SerializeField]
+    int sceneIndex;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RestartLevel(sceneIndex); //när knappen trycks på så förs man till scenen man har skrivit i scen - Darian
     }
+
+    public void RestartLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex); //detta laddar scenen man förts till - Darian
+    }
+
 }
