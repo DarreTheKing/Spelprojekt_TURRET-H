04712202dfbca423d9a7873e1e6bc3fdcd97ee09
OtisHildebrand <otis.hildebrand@elev.ga.lbs.se>
2022-01-18 14:21:19 +0100

    Camera, kanske crashar

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public int cameraDirection;
+    new Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = gameObject.AddComponent<Camera>();
+        camera.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(cameraDirection == 1)
+        {
+            Instantiate(camera, transform.position + new Vector3(0, -10, -10), Quaternion.identity);
+        }
+        else if (cameraDirection == 2)
+        {
+            Instantiate(camera, transform.position + new Vector3(0, 10, -10), Quaternion.identity);
+        }
+        else if (cameraDirection == 3)
+        {
+            Instantiate(camera, transform.position + new Vector3(18, 0, -10), Quaternion.identity);
+        }
+        else if (cameraDirection == 4)
+        {
+            Instantiate(camera, transform.position + new Vector3(-18, 0, -10), Quaternion.identity);
+        }
+    }
+}
