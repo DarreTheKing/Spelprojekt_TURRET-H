efd0667df85c52fffa81d26ed561205683ca9184
OtisHildebrand <otis.hildebrand@elev.ga.lbs.se>
2022-01-19 00:06:19 +0100

    Minion

--- Assets/Script/Win.cs
+++ Assets/Script/Win.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Win : MonoBehaviour
 {
-    WinImage win;
+    public Image winImage;
     // Start is called before the first frame update
     void Start()
     {
-        win = FindObjectOfType<WinImage>();
     }
 
     // Update is called once per frame
@@ -20,7 +20,8 @@ public class Win : MonoBehaviour
     {
         if(other.gameObject.tag == ("Player"))
         {
-            win.enabled = true;
+            winImage.enabled = true;
         }
     }
+
 }
