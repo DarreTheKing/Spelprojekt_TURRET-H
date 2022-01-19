d1ac646a0934c12d8b68835d47fd0feeabd21a95
DarreTheKing <darian.belvert@elev.ga.lbs.se>
2022-01-19 12:41:25 +0100

    Options

---
+++ Assets/Script/Scene_Changer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Changer : MonoBehaviour
+{
+    [SerializeField]
+    int sceneIndex;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RestartLevel(sceneIndex); //när knappen trycks på så förs man till scenen man har skrivit i scen - Darian
+    }
+
+    public void RestartLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex); //detta laddar scenen man förts till - Darian
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main_menu_Scene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/Win.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Win : MonoBehaviour
+{
+    public Image winImage;
+    public Image replayImage;
+    public Image menuImage;
+    public Button replayButton;
+    public Button menuButton;
+    public Text menuText;
+    public Text replayText;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == ("Player"))
+        {
+            winImage.enabled = true;
+            replayButton.enabled = true;
+            replayImage.enabled = true;
+            replayText.enabled = true;
+            menuImage.enabled = true;
+            menuButton.enabled = true;
+            menuText.enabled = true;
+        }
+    }
+
+}
