623e8bd3ae8f997e69e3aac927fec140629fa6a3
OtisHildebrand <otis.hildebrand@elev.ga.lbs.se>
2022-01-20 01:37:09 +0100

    ?

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -6,11 +6,11 @@ public class Spawner : MonoBehaviour
 {
     private float timer;
     public GameObject enemy;
-    public static int enemyAmmount = 5;
+    public static int enemyAmmount = 3;
     // Start is called before the first frame update
     void Start()
     {
-        timer = Random.Range(2, 5);
+        timer = Random.Range(5, 10);
     }
 
     // Update is called once per frame
@@ -19,7 +19,7 @@ public class Spawner : MonoBehaviour
         timer -= Time.deltaTime;
         if(timer <= 0)
         {
-            timer = Random.Range(2, 5);
+            timer = Random.Range(5, 10);
             enemyAmmount -= 1;
             Instantiate(enemy, transform.position, Quaternion.identity);
         }
