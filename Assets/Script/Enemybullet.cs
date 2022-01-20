29300a26013084c3c7dc60d143b72732dc1ed6c6
Zakk <zakk.norman@elev.ga.lbs.se>
2022-01-20 15:01:54 +0100

    fix darians shit

--- Assets/Script/Enemybullet.cs
+++ Assets/Script/Enemybullet.cs
@@ -16,7 +16,9 @@ public class Enemybullet : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
         direction = (player.transform.position - transform.position).normalized * speed; 
-        rb.velocity = new Vector2(direction.x, direction.y); 
+        rb.velocity = new Vector2(direction.x, direction.y);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     private void Update()
