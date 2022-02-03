fdaac709afbb88ec30ba93c60fbee0153798e9c7
DarreTheKing <darian.belvert@elev.ga.lbs.se>
2022-02-03 12:52:17 +0100

    Paus meny

---
+++ Assets/Script/Options.cs
@@ -0,0 +1,15 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    public AudioMixer mainMixer; //Skapar en audiomixer - Darian
+    public void SetVolume(float Volume)
+    {
+        mainMixer.SetFloat("Volume", Volume); //gör så att man kan ändra volymen - Darian
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
