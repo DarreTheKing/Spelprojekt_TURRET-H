3d36d2237ec9db9fe9eca88e432d09bcdc4eef25
OtisHildebrand <otis.hildebrand@elev.ga.lbs.se>
2022-02-15 09:58:50 +0100

    Prefabs

--- Assets/Script/AddEnemies.cs
+++ Assets/Script/AddEnemies.cs
@@ -1,28 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Kod av Otis
 public class AddEnemies : MonoBehaviour
 {
+    //Variabel för vad som ska spawnas
     public GameObject enemySpawner;
+    //Variabeln för om det tidigare hade spawnats fiender
     private int enemycount = 1;
+    //Variabel för vilket håll fienderna ska spawnas åt
     public int direction;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //Kollar om spawnern har spawnat fiender
         if(enemycount == 1)
         {
             enemycount -= 1;
+            //Spawnar fyra spawners i varje hörn, beroende på hållet spawnas de olika
             if (direction == 1)
             {
                 
@@ -31,7 +25,7 @@ public class AddEnemies : MonoBehaviour
                 Instantiate(enemySpawner, transform.position + new Vector3(-16, 14, 0), Quaternion.identity);
                 Instantiate(enemySpawner, transform.position + new Vector3(-16, -2, 0), Quaternion.identity);
             }
-            if (direction == 2)
+            else if (direction == 2)
             {
 
                 Instantiate(enemySpawner, transform.position + new Vector3(16, -14, 0), Quaternion.identity);
@@ -39,7 +33,7 @@ public class AddEnemies : MonoBehaviour
                 Instantiate(enemySpawner, transform.position + new Vector3(-16, -14, 0), Quaternion.identity);
                 Instantiate(enemySpawner, transform.position + new Vector3(-16, 2, 0), Quaternion.identity);
             }
-            if (direction == 3)
+            else if (direction == 3)
             {
 
                 Instantiate(enemySpawner, transform.position + new Vector3(2, 8, 0), Quaternion.identity);
@@ -47,7 +41,7 @@ public class AddEnemies : MonoBehaviour
                 Instantiate(enemySpawner, transform.position + new Vector3(-26, -8, 0), Quaternion.identity);
                 Instantiate(enemySpawner, transform.position + new Vector3(-26, 8, 0), Quaternion.identity);
             }
-            if (direction == 4)
+            else if (direction == 4)
             {
 
                 Instantiate(enemySpawner, transform.position + new Vector3(-2, 8, 0), Quaternion.identity);
